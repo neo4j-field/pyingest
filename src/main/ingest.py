7ad13571361eec7e704d76bbdd3ad477535ab8ab
Matt Holford <matt.holford@neo4j.com>
2019-10-21 16:31:21 -0700

    First check in

---
+++ src/main/ingest.py
@@ -0,0 +1,203 @@
+import pandas as pd
+from neo4j import GraphDatabase
+import yaml
+import datetime
+import sys
+import gzip
+from zipfile import ZipFile
+from urllib.parse import urlparse
+import boto3
+from smart_open import open
+import io
+import pathlib
+import ijson
+
+config = dict()
+supported_compression_formats = ['gzip', 'zip', 'none']
+
+
+class LocalServer(object):
+
+    def __init__(self):
+        self._driver = GraphDatabase.driver(config['server_uri'],
+                                            auth=(config['admin_user'],
+                                                  config['admin_pass']))
+
+    def close(self):
+        self._driver.close()
+
+    def load_file(self, file):
+        # Set up parameters/defaults
+        # Check skip_file first so we can exit early
+        skip = file.get('skip_file') or False
+        if skip:
+            print("Skipping this file: {}", file['url'])
+            return
+
+        print("{} : Reading file", datetime.datetime.utcnow())
+
+        # If file type is specified, use that.  Else check the extension.  Else, treat as csv
+        type = file.get('type') or 'NA'
+        if type != 'NA':
+            if type == 'csv':
+                self.load_csv(file)
+            elif type == 'json':
+                self.load_json(file)
+            else:
+                print("Error! Can't process file because unknown type", type, "was specified")
+        else:
+            file_suffixes = pathlib.Path(file['url']).suffixes
+            if '.csv' in file_suffixes:
+                self.load_csv(file)
+            elif '.json' in file_suffixes:
+                self.load_json(file)
+            else:
+                self.load_csv(file)
+
+    # Tells ijson to return decimal number as float.  Otherwise, it wraps them in a Decimal object,
+    # which angers the Neo4j driver
+    @staticmethod
+    def ijson_decimal_as_float(events):
+        for prefix, event, value in events:
+            if event == 'number':
+                value = str(value)
+            yield prefix, event, value
+
+    def load_json(self, file):
+        with self._driver.session() as session:
+            params = self.get_params(file)
+            openfile = file_handle(params['url'], params['compression'])
+            # 'item' is a magic word in ijson.  It just means the next-level element of an array
+            items = ijson.common.items(self.ijson_decimal_as_float(ijson.parse(openfile)), 'item')
+            # Next, pool these into array of 'chunksize'
+            halt = False
+            rec_num = 0
+            chunk_num = 0
+            rows = []
+            while not halt:
+                row = next(items, None)
+                if row is None:
+                    halt = True
+                else:
+                    rec_num = rec_num+1;
+                    if rec_num > params['skip_records']:
+                        rows.append(row)
+                        if len(rows) == params['chunk_size']:
+                            print(file['url'], chunk_num, datetime.datetime.utcnow(), flush=True)
+                            chunk_num = chunk_num + 1
+                            rows_dict = {'rows': rows}
+                            session.run(statement=params['cql'], dict=rows_dict).consume()
+                            rows = []
+
+            if len(rows) > 0:
+                print(file['url'], chunk_num, datetime.datetime.utcnow(), flush=True)
+                rows_dict = {'rows': rows}
+                session.run(statement=params['cql'], dict=rows_dict).consume()
+
+    @staticmethod
+    def get_params(file):
+        params = dict()
+        params['skip_records'] = file.get('skip_records') or 0
+        params['compression'] = file.get('compression') or 'none'
+        if params['compression'] not in supported_compression_formats:
+            print("Unsupported compression format: {}", params['compression'])
+
+        params['url'] = file['url']
+        print("File {}", params['url'])
+        params['cql'] = file['cql']
+        params['chunk_size'] = file.get('chunk_size') or 1000
+        params['field_sep'] = file.get('field_separator') or ','
+        return params
+
+    def load_csv(self, file):
+        with self._driver.session() as session:
+            params = self.get_params(file)
+            openfile = file_handle(params['url'], params['compression'])
+
+            # - The file interfaces should be consistent in Python but they aren't
+            if params['compression'] == 'zip':
+                header = openfile.readline().decode('UTF-8')
+            else:
+                header = str(openfile.readline())
+
+            # Grab the header from the file and pass that to pandas.  This allow the header
+            # to be applied even if we are skipping lines of the file
+            header = header.strip().split(params['field_sep'])
+
+            # Pandas' read_csv method is highly optimized and fast :-)
+            row_chunks = pd.read_csv(openfile, dtype=str, sep=params['field_sep'], error_bad_lines=False,
+                                     index_col=False, skiprows=params['skip_records'], names=header,
+                                     low_memory=False, engine='c', compression='infer', header=None,
+                                     chunksize=params['chunk_size'])
+
+            for i, rows in enumerate(row_chunks):
+                print(params['url'], i, datetime.datetime.utcnow(), flush=True)
+                # Chunk up the rows to enable additional fastness :-)
+                rows_dict = {'rows': rows.fillna(value="").to_dict('records')}
+                session.run(statement=params['cql'],
+                            dict=rows_dict).consume()
+
+        print("{} : Completed file", datetime.datetime.utcnow())
+
+    def pre_ingest(self):
+        if 'pre_ingest' in config:
+            statements = config['pre_ingest']
+
+            with self._driver.session() as session:
+                for statement in statements:
+                    session.run(statement=statement)
+
+    def post_ingest(self):
+        if 'post_ingest' in config:
+            statements = config['post_ingest']
+
+            with self._driver.session() as session:
+                for statement in statements:
+                    session.run(statement=statement)
+
+
+def file_handle(url, compression):
+    parsed = urlparse(url)
+    if parsed.scheme == 's3':
+        path = get_s3_client().get_object(Bucket=parsed.netloc, Key=parsed.path[1:])['Body']
+    else:
+        path = parsed.path
+    if compression == 'gzip':
+        return gzip.open(path, 'rt')
+    elif compression == 'zip':
+        # Only support single file in ZIP archive for now
+        if isinstance(path, str):
+            buffer = path
+        else:
+            buffer = io.BytesIO(path.read())
+        zf = ZipFile(buffer)
+        filename= zf.infolist()[0].filename
+        return zf.open(filename)
+    else:
+        return open(path)
+
+
+def get_s3_client():
+    return boto3.Session().client('s3')
+
+
+def load_config(configuration):
+    global config
+    with open(configuration) as config_file:
+        config = yaml.load(config_file)
+
+
+def main():
+    configuration = sys.argv[1]
+    load_config(configuration)
+    server = LocalServer()
+    server.pre_ingest()
+    file_list = config['files']
+    for file in file_list:
+        server.load_file(file)
+    server.post_ingest()
+    server.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/java/org/mholford/pyingest/IngestPyIT.java
@@ -0,0 +1,386 @@
+package org.mholford.pyingest;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.driver.v1.*;
+import org.neo4j.harness.ServerControls;
+import org.neo4j.harness.internal.EnterpriseInProcessServerBuilder;
+import org.neo4j.test.rule.TestDirectory;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Integration tests for the Python script (ingest.py).  To test various scenarios,
+ * there are multiple folder in test/resources with separate config.yml.  The IT works by:
+ * <ul>
+ *   <li>Starting up an in-proc embedded Neo4j server</li>
+ *   <li>Rewriting the config.yml using the boltURI of the embedded server (this is indeterminate
+ *   until it starts) and the absolute file paths of the test files.  The rewritten config
+ *   (altered-config.yml) should not be changed and it is not checked into the project repo</li>
+ *   <li>Running the test python ingest script in a separate Process using the rewritten config</li>
+ *   <li>Inspecting the ingested data in the embedded server</li>
+ * </ul>
+ */
+public class IngestPyIT {
+  private ServerControls server;
+  private Driver driver;
+  private final AuthToken BASIC_AUTH = AuthTokens.basic("neo4j", "neo4j");
+
+  @Rule
+  public final TestDirectory testDirectory = TestDirectory.testDirectory();
+
+  @Before
+  public void setup() {
+    File directory = testDirectory.directory();
+    server = new EnterpriseInProcessServerBuilder(directory).newServer();
+    driver = GraphDatabase.driver(server.boltURI(), BASIC_AUTH);
+  }
+
+  @Test
+  public void testRegularIngestCSV() {
+    String folder = "plain-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testRegularIngestJSON() {
+    String folder = "plain-ingest-json";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testGzipIngestCSV() {
+    String folder = "gzip-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testGzipIngestJSON() {
+    String folder = "gzip-ingest-json";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testZipIngestCSV() {
+    String folder = "zip-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testZipIngestJSON() {
+    String folder = "zip-ingest-json";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testSkipFileIngest() {
+    String folder = "skip-file-ingest";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 0, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testSkipLinesIngestCSV() {
+    String folder = "skip-lines-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 7, 3);
+      checkResults("BLabel", "b", 5, 5);
+      checkResults("CLabel", "c", 2, 8);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testSkipLinesIngestJSON() {
+    String folder = "skip-lines-ingest-json";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 7, 3);
+      checkResults("BLabel", "b", 5, 5);
+      checkResults("CLabel", "c", 2, 8);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testChunksizeIngestCSV() {
+    String folder = "chunksize-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      String in = runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+      assertEquals(3, countMatchesIn(in, ".*b\\.csv.*"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testChunksizeIngestJSON() {
+    String folder = "chunksize-ingest-json";
+    try {
+      rewriteConfig(folder);
+      String in = runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+      assertEquals(3, countMatchesIn(in, ".*b\\.json.*"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testFieldsepIngest() {
+    String folder = "fieldsep-ingest";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testPrePostIngest() {
+    String folder = "prepost-ingest";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+      checkResults("DLabel", "d", 1, 0);
+      checkResults("ELabel", "e", 1, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void testS3IngestCSV() {
+    String folder = "s3-ingest-csv";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testS3IngestJSON() {
+    String folder = "s3-ingest-json";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testExplicitTypeIngest() {
+    String folder = "explicit-type-ingest";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+      checkResults("ALabel", "a", 10, 0);
+      checkResults("BLabel", "b", 10, 0);
+      checkResults("CLabel", "c", 10, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void testNumericValues() {
+    String folder = "numbers-ingest";
+    try {
+      rewriteConfig(folder);
+      runPythonIngest(folder);
+
+      Session session = driver.session();
+      Record rec = session.run("MATCH (x:ALabel) return x").list().get(0);
+      assertEquals("47", rec.get("x").get("p1").asString());
+      assertEquals("5.23456", rec.get("x").get("p2").asString());
+      assertEquals("123456789012345", rec.get("x").get("p3").asString());
+
+      rec = session.run("MATCH (x:BLabel) return x").list().get(0);
+      assertEquals("47", rec.get("x").get("p1").asString());
+      assertEquals("5.23456", rec.get("x").get("p2").asString());
+      assertEquals("123456789012345", rec.get("x").get("p3").asString());
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @After
+  public void tearDown() {
+    server.close();
+  }
+
+  private void checkResults(String label, String prefix, int numRecs, int start) {
+    Session session = driver.session();
+    String q = fmt("MATCH (x:%s) return x order by x.p1", label);
+    List<Record> results = session.run(q).list();
+    assertEquals(numRecs, results.size());
+    for (int i = 0; i < numRecs; i++) {
+      Record rec = results.get(i);
+      for (int j = 0; j < 3; j++) {
+        assertEquals(fmt("%s%d%d", prefix, start+i, j), rec.get("x").get(fmt("p%d", j+1)).asString());
+      }
+    }
+  }
+
+  private String runPythonIngest(String folder) throws IOException, InterruptedException {
+    Process proc = new ProcessBuilder("python3", "main/ingest.py",
+            fmt("test/resources/%s/altered-config.yml", folder))
+            .directory(new File("src")).redirectErrorStream(true).start();
+    proc.waitFor();
+    InputStream procStream = proc.getInputStream();
+
+    StringWriter writer = new StringWriter();
+    IOUtils.copy(procStream, writer, Charset.forName("UTF-8"));
+    String msg = writer.toString();
+    System.out.println(msg);
+    return msg;
+  }
+
+  private int countMatchesIn(String in, String match) {
+    Pattern pattern = Pattern.compile(match);
+    int count = 0;
+    try (BufferedReader br = new BufferedReader(new StringReader(in))) {
+      String s;
+      while ((s = br.readLine()) != null){
+        Matcher matcher = pattern.matcher(s);
+        if (matcher.matches()) {
+          count++;
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail();
+    }
+    return count;
+  }
+
+  private void rewriteConfig(String folder) throws IOException {
+    String userDir = System.getProperty("user.dir");
+    String config = fileToString(fmt("%s/src/test/resources/%s/config.yml", userDir, folder));
+    Map<String, String> subs = Map.of("boltURI", server.boltURI().toString(),
+            "testResourceDir", Path.of("src/test/resources").toAbsolutePath().toString());
+    config = new StrSubstitutor(subs).replace(config);
+    Files.write(Path.of(fmt("%s/src/test/resources/%s/altered-config.yml", userDir, folder)),
+            config.getBytes());
+  }
+
+  private String fileToString(String path) throws IOException {
+    return Files.readString(Path.of(path));
+  }
+
+  private static String fmt(String orig, Object... subs) {
+    return String.format(orig, (Object[]) subs);
+  }
+}

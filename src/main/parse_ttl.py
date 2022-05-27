daba641383d19b1af6c2b09d00783e334ffbbb00
Cuneyt Tyler <cuneyttyler@gmail.com>
2022-05-27 12:14:38 +0300

    Parse TTL

--- src/main/parse_ttl.py
+++ src/main/parse_ttl.py
@@ -1,67 +1,87 @@
+import re
+
 class TTLParser():
     class InvalidTTLDocument(Exception):
         pass
 
+    def split_and_keep(self,seperator, s, maxsplit=0):
+        TEMP_PLACEHOLDER = ';"_;"_'
+        return re.split(TEMP_PLACEHOLDER, re.sub(seperator, lambda match: TEMP_PLACEHOLDER + match.group() , s),maxsplit=maxsplit)
+
     def parse_ttl_line(self, line, new_rec, prev_subject,prefixes):
-        cols = line.strip().split(' ')
+        if len(line.strip()) == 0:
+            return None
 
-        row = {}
-        if new_rec:
-            if len(cols) != 4:
-                raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
+        try:
+            cols = self.split_and_keep(r' (".*"|<http)',line.strip(),maxsplit=2)
+            cols = [col.strip() for col in cols]
 
-            subjectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[0]]
-            if len(subjectPrefix) == 1:
-                row['subject'] = cols[0].replace(subjectPrefix[0]['alias'],subjectPrefix[0]['prefix'])
+            cols2 = self.split_and_keep(r'@[a-z]{2} \.|@[a-z]{2} ;', cols[2])
+            cols[2] = cols2[0]
+            if len(cols2) > 1:
+                cols3 = re.split(r'@[a-z]{2} ', cols2[1])
+                cols.append(cols3[1])
+
+            row = {}
+            if new_rec:
+                if len(cols) != 4:
+                    raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
+
+                subjectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[0]]
+                if len(subjectPrefix) == 1:
+                    row['subject'] = cols[0].replace(subjectPrefix[0]['alias'],subjectPrefix[0]['prefix'])
+                else:
+                    row['subject'] = self.check_first_and_last_char(cols[0])
+
+                predicatePrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[1]]
+                if len(predicatePrefix) == 1:
+                    row['predicate'] = cols[1].replace(predicatePrefix[0]['alias'],predicatePrefix[0]['prefix'])
+                else:
+                    row['predicate'] = self.check_first_and_last_char(cols[1])
+
+                objectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[2]]
+                if len(objectPrefix) == 1:
+                    row['object'] = cols[2].replace(objectPrefix[0]['alias'],objectPrefix[0]['prefix'])
+                else:
+                    row['object'] = self.check_first_and_last_char(cols[2])
+
+                if cols[3] == ';':
+                    new_rec = False
+                    prev_subject = row['subject']
+                elif cols[3] == '.':
+                    new_rec = True
+                else:
+                    raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
             else:
-                row['subject'] = self.check_first_and_last_char(cols[0])
+                if len(cols) != 3:
+                    raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
 
-            predicatePrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[1]]
-            if len(predicatePrefix) == 1:
-                row['predicate'] = cols[1].replace(predicatePrefix[0]['alias'],predicatePrefix[0]['prefix'])
-            else:
-                row['predicate'] = self.check_first_and_last_char(cols[1])
+                row['subject'] = prev_subject
 
-            objectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[2]]
-            if len(objectPrefix) == 1:
-                row['object'] = cols[2].replace(objectPrefix[0]['alias'],objectPrefix[0]['prefix'])
-            else:
-                row['object'] = self.check_first_and_last_char(cols[2])
+                predicatePrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[0]]
+                if len(predicatePrefix) == 1:
+                    row['predicate'] = cols[0].replace(predicatePrefix[0]['alias'], predicatePrefix[0]['prefix'])
+                else:
+                    row['predicate'] = self.check_first_and_last_char(cols[0])
 
-            if cols[3] == ';':
-                new_rec = False
-                prev_subject = row['subject']
-            elif cols[3] == '.':
-                new_rec = True
-            else:
-                raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
-        else:
-            if len(cols) != 3:
-                raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
+                objectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[1]]
+                if len(objectPrefix) == 1:
+                    row['object'] = cols[1].replace(objectPrefix[0]['alias'], objectPrefix[0]['prefix'])
+                else:
+                    row['object'] = self.check_first_and_last_char(cols[1])
 
-            row['subject'] = prev_subject
+                if cols[2] == ';':
+                    new_rec = False
+                elif cols[2] == '.':
+                    new_rec = True
+                    prev_subject = None
+                else:
+                    raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
 
-            predicatePrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[0]]
-            if len(predicatePrefix) == 1:
-                row['predicate'] = cols[0].replace(predicatePrefix[0]['alias'], predicatePrefix[0]['prefix'])
-            else:
-                row['predicate'] = self.check_first_and_last_char(cols[0])
-
-            objectPrefix = [prefix for prefix in prefixes if prefix['alias'] in cols[1]]
-            if len(objectPrefix) == 1:
-                row['object'] = cols[1].replace(objectPrefix[0]['alias'], objectPrefix[0]['prefix'])
-            else:
-                row['object'] = self.check_first_and_last_char(cols[1])
-
-            if cols[2] == ';':
-                new_rec = False
-            elif cols[2] == '.':
-                new_rec = True
-                prev_subject = None
-            else:
-                raise self.InvalidTTLDocument("Error! Invalid TTL Document!")
-
-        return row, new_rec, prev_subject
+            return row, new_rec, prev_subject
+        except:
+            print('Line: %s' % line)
+            raise
 
     def parse_ttl_prefix(self,line):
         cols = line.strip().split(' ')
@@ -100,6 +120,10 @@ class TTLParser():
                 continue
 
             row, new_rec, prev_subject = self.parse_ttl_line(self.line, new_rec, prev_subject, prefixes)
+            if not row:
+                self.line = file.readline()
+                continue
+
             rows.append(row)
 
             if last_subject != row['subject'] and len(rows) >= chunk_size:
@@ -113,6 +137,11 @@ class TTLParser():
 
     def check_first_and_last_char(self,str):
         str = str[1:] if str[0] == '<' else str
-        str = str[:-1] if str[len(str)-1] == '>' else str
+        str = str[:-1] if str[len(str) - 1] == '>' else str
+
+        if str[0] == '"':
+            str = str[1:]
+            str = str[:-1]
+
 
         return str
